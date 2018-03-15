e7d52112fd0408c29ec61a45c56ed4f5241509bf
Oliver Stadie <hello@waog.net>
2018-03-15 02:17:12 +0100

    ADD Http Requests

--- build/sources/main/java/net/waog/text2cb/MyForgeEventHandler.java
+++ build/sources/main/java/net/waog/text2cb/MyForgeEventHandler.java
@@ -3,6 +3,11 @@ package net.waog.text2cb;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
@@ -21,6 +26,7 @@ public class MyForgeEventHandler {
 			this.lastMessage = message;
 			// writeToChat(message);
 		}
+		callHttp(event.getItem().getItem().getDisplayName(), "collect");
 		copyToClipBoard(message);
 	}
 
@@ -38,30 +44,46 @@ public class MyForgeEventHandler {
 		thread.start();
 	}
 
-	private void writeToChat(String text) {
-		Runnable myRunnable = new Runnable() {
-			public void run() {
-				Minecraft.getMinecraft().player.sendChatMessage(text);
-			}
-		};
-
-		Thread thread = new Thread(myRunnable);
-		thread.start();
-	}
-
 	@SubscribeEvent
 	public void OnFinish(PlayerInteractEvent.RightClickBlock e) {
 		logAndPlay(e.getItemStack().getDisplayName() + " used!");
+		callHttp(e.getItemStack().getDisplayName(), "useOnBlock");
 	}
 
 	@SubscribeEvent
 	public void OnFinish(PlayerInteractEvent.RightClickItem e) {
 		logAndPlay(e.getItemStack().getDisplayName() + " used!");
+		callHttp(e.getItemStack().getDisplayName(), "useItem");
 	}
 
 	private void logAndPlay(String message) {
 		// writeToChat(message);
 		copyToClipBoard(message);
+	}
+
+	private void callHttp(String item, String activity) {
+		try {
+			String url = "http://localhost:3000/play"
+					+ "/" + URLEncoder.encode(item, "UTF-8")
+					+ "/" + Minecraft.getMinecraft().gameSettings.language
+					+ "/" + URLEncoder.encode(activity, "UTF-8");
+			
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			// optional default is GET
+			con.setRequestMethod("GET");
+
+			//add request header
+			con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
